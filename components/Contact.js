10fa5d86ec93cdaad957142ba76ae6dea623dce3
krzysztofhejna <khejna15@wp.pl>
2019-01-20 17:11:19 +0100

    Create contacts app

---
+++ components/Contact.js
@@ -0,0 +1,17 @@
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+      React.createElement('div', {className: 'contactItem'},
+        React.createElement('i', {
+          className: 'fas fa-user',
+        }),
+        React.createElement('p', {className: 'contactLabel'}, this.props.item.firstName + ' ' + this.props.item.lastName),
+        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email}, this.props.item.email),
+      )
+    )
+  },
+});
